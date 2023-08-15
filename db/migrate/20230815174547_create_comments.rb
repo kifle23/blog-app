586ea8d980fbd9b064c708462c123fa9c70d15cc
kifle23 <kiflekifle23@gmail.com>
2023-08-15 20:55:04 +0300

    Update migration CreateComments

--- db/migrate/20230815174547_create_comments.rb
+++ db/migrate/20230815174547_create_comments.rb
@@ -1,10 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :author, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
       t.text :text
-
       t.timestamps
     end
   end
