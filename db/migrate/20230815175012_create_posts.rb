f89571c4dfa7f7f752363bae137c0e6dee2e3108
kifle23 <kiflekifle23@gmail.com>
2023-08-15 20:50:45 +0300

    Generate migration CreatePosts

---
+++ db/migrate/20230815175012_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.references :author, null: false, foreign_key: true
+      t.string :title
+      t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+
+      t.timestamps
+    end
+  end
+end
