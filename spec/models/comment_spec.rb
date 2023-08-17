608d137eeac00b65b4763db2d669299734a234e1
kifle23 <kiflekifle23@gmail.com>
2023-08-17 14:47:42 +0300

    Fix offences

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,29 +8,31 @@ describe Comment, type: :model do
 
   describe 'callbacks' do
     let!(:user) { User.create(name: 'Ruby Guy', posts_count: 0) }
-  
+
     it 'updates the comments counter of the associated post after create' do
       post = Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author: user)
-      comment = Comment.new(post: post, author: user, text: 'Test1')
-  
-      expect {
+      comment = Comment.new(post:, author: user, text: 'Test1')
+
+      expect do
         comment.save!
         post.reload
-      }.to change { post.comments_counter }.by(1)
+      end.to change { post.comments_counter }.by(1)
     end
   end
 
   describe 'private methods' do
     describe '#update_post_comments_counter' do
       let!(:author) { User.create!(name: 'Ruby Guy') }
-      let!(:post) { Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author: author) }
-      let(:comment) { Comment.new(post: post, author: author) }
-  
+      let!(:post) do
+        Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author:)
+      end
+      let(:comment) { Comment.new(post:, author:) }
+
       it 'updates the comments counter of the associated post' do
-        expect {
+        expect do
           comment.save!
           post.reload
-        }.to change { post.comments_counter }.by(1)
+        end.to change { post.comments_counter }.by(1)
       end
     end
   end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -11,26 +11,28 @@ describe Like, type: :model do
 
     it 'updates the likes counter of the associated post after create' do
       post = Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author: user)
-      like = Like.new(post: post, author: user)
+      like = Like.new(post:, author: user)
 
-      expect {
+      expect do
         like.save!
         post.reload
-      }.to change { post.likes_counter }.by(1)
+      end.to change { post.likes_counter }.by(1)
     end
   end
 
   describe 'private methods' do
     describe '#update_post_likes_counter' do
       let!(:author) { User.create!(name: 'Ruby Guy') }
-      let!(:post) { Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author: author) }
-      let(:like) { Like.new(post: post, author: author) }
+      let!(:post) do
+        Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author:)
+      end
+      let(:like) { Like.new(post:, author:) }
 
       it 'updates the likes counter of the associated post' do
-        expect {
+        expect do
           like.save!
           post.reload
-        }.to change { post.likes_counter }.by(1)
+        end.to change { post.likes_counter }.by(1)
       end
     end
   end
