ecce3ddcd973dd4ad6a8f719af37b14eb57e4739
kifle23 <kiflekifle23@gmail.com>
2023-08-17 14:26:08 +0300

    Add spec for model Post-validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,11 @@ describe Post, type: :model do
       it { is_expected.to have_many(:comments) }
       it { is_expected.to have_many(:likes) }
     end
+  
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:title) }
+      it { is_expected.to validate_length_of(:title).is_at_most(250) }
+      it { is_expected.to validate_numericality_of(:comments_counter).only_integer.is_greater_than_or_equal_to(0) }
+      it { is_expected.to validate_numericality_of(:likes_counter).only_integer.is_greater_than_or_equal_to(0) }
+    end
 end
