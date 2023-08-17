ccfd2c03e8867cdb4152248322747d6f641d08fe
kifle23 <kiflekifle23@gmail.com>
2023-08-17 14:05:52 +0300

    Add spec for model Comment-callbacks

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,5 +5,18 @@ describe Comment, type: :model do
     it { is_expected.to belong_to(:author).class_name('User') }
     it { is_expected.to belong_to(:post) }
   end
- 
+
+  describe 'callbacks' do
+    let!(:user) { User.create(name: 'Ruby Guy', posts_count: 0) }
+  
+    it 'updates the comments counter of the associated post after create' do
+      post = Post.create!(title: 'Test1', text: 'This is test', comments_counter: 0, likes_counter: 0, author: user)
+      comment = Comment.new(post: post, author: user, text: 'Test1')
+  
+      expect {
+        comment.save!
+        post.reload
+      }.to change { post.comments_counter }.by(1)
+    end
+  end
 end
