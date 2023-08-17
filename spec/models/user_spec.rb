b98f3be9a46d09bcf5646a3f8927ab02a217610f
kifle23 <kiflekifle23@gmail.com>
2023-08-17 14:44:22 +0300

    Add spec for model User-validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,9 @@ describe User, type: :model do
       it { is_expected.to have_many(:comments).with_foreign_key(:author_id) }
       it { is_expected.to have_many(:likes).with_foreign_key(:author_id) }
     end
+  
+    describe 'validations' do
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_numericality_of(:posts_count).only_integer.is_greater_than_or_equal_to(0) }
+    end
 end
