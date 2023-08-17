03a3c18fb9a19707f8e330d83c7d6ba87819aa9b
kifle23 <kiflekifle23@gmail.com>
2023-08-17 11:55:42 +0300

    Add validation for Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,10 @@ class Post < ApplicationRecord
 
   after_create :update_author_posts_count
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
