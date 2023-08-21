3b4dae5a83a616c4c181b60563a72fcb1bb66240
kifle23 <kiflekifle23@gmail.com>
2023-08-21 17:40:47 +0300

    Show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
     def index
+      @user = User.find(params[:user_id])
     end
   
     def show
+      @post = User.find(params[:user_id]).posts.find(params[:id])
     end
 end
   
