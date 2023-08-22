cda9be19ca790c860374959cf6a6e1fd65cc40ef
kifle23 <kiflekifle23@gmail.com>
2023-08-22 16:27:32 +0300

    Update specs to use object data

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,35 +1,39 @@
 require 'rails_helper'
+
 describe PostsController, type: :request do
+  let!(:user) { User.create(name: 'Tom') }
+  let!(:post) { Post.create(author: user, title: 'This is my first post') }
+
   describe '#index' do
     it 'returns a successful response' do
-      get '/users/1/posts'
+      get "/users/#{user.id}/posts"
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the index template' do
-      get '/users/1/posts'
+      get "/users/#{user.id}/posts"
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/1/posts'
+      get "/users/#{user.id}/posts"
       expect(response.body).to include('Tom')
     end
   end
 
   describe '#show' do
     it 'returns a successful response' do
-      get '/users/1/posts/1'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the show template' do
-      get '/users/1/posts/1'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get '/users/1/posts/1'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response.body).to include('This is my first post')
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,37 +1,42 @@
 require 'rails_helper'
+
 describe 'UsersControllers', type: :request do
   describe 'GET #index' do
     it 'returns a successful response' do
+      allow(User).to receive(:all).and_return([])
       get '/users'
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the index template' do
+      allow(User).to receive(:all).and_return([])
       get '/users'
       expect(response).to render_template(:index)
     end
 
     it 'includes correct placeholder text in the response body' do
+      allow(User).to receive(:all).and_return([User.new(name: 'Tom')])
       get '/users'
       expect(response.body).to include('Tom')
     end
   end
 
   describe 'GET #show' do
-    let(:user) { User.create(name: 'Ruby Guy') }
-
     it 'returns a successful response' do
-      get "/users/#{user.id}"
+      allow(User).to receive(:find).and_return(User.new(name: 'Ruby Guy'))
+      get '/users/1'
       expect(response).to have_http_status(:success)
     end
 
     it 'renders the show template' do
-      get "/users/#{user.id}"
+      allow(User).to receive(:find).and_return(User.new(name: 'Ruby Guy'))
+      get '/users/1'
       expect(response).to render_template(:show)
     end
 
     it 'includes correct placeholder text in the response body' do
-      get "/users/#{user.id}"
+      allow(User).to receive(:find).and_return(User.new(name: 'Ruby Guy'))
+      get '/users/1'
       expect(response.body).to include('Ruby Guy')
     end
   end
