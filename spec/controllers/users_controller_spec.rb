b1f6bb0e979472895ad66ad3bf42da301269b14a
kifle23 <kiflekifle23@gmail.com>
2023-08-22 12:35:29 +0300

    Spec for UsersController#index->render template

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,5 +5,10 @@ describe "UsersControllers", type: :request do
         get '/users'
         expect(response).to have_http_status(:success)
       end
+
+      it "renders the index template" do
+        get '/users'
+        expect(response).to render_template(:index)
+      end
     end
 end
