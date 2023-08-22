ddb41336a58ac5f6d08cc0184e91a9e6ded0aa13
kifle23 <kiflekifle23@gmail.com>
2023-08-22 12:43:07 +0300

    Fix offences

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,36 +1,36 @@
 require 'rails_helper'
 describe PostsController, type: :request do
-    describe '#index' do
-      it 'returns a successful response' do
-        get '/users/1/posts' 
-        expect(response).to have_http_status(:success)
-      end
-
-      it 'renders the index template' do
-        get '/users/1/posts' 
-        expect(response).to render_template(:index)
-      end
-
-      it 'includes correct placeholder text in the response body' do
-        get '/users/1/posts' 
-        expect(response.body).to include("Tom")
-      end
+  describe '#index' do
+    it 'returns a successful response' do
+      get '/users/1/posts'
+      expect(response).to have_http_status(:success)
     end
 
-    describe '#show' do
-      it 'returns a successful response' do
-        get '/users/1/posts/1'
-        expect(response).to have_http_status(:success)
-      end
-
-      it 'renders the show template' do
-        get '/users/1/posts/1'
-        expect(response).to render_template(:show)
-      end
-
-      it 'includes correct placeholder text in the response body' do
-        get '/users/1/posts/1'
-        expect(response.body).to include('This is my first post')
-      end
+    it 'renders the index template' do
+      get '/users/1/posts'
+      expect(response).to render_template(:index)
     end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users/1/posts'
+      expect(response.body).to include('Tom')
+    end
+  end
+
+  describe '#show' do
+    it 'returns a successful response' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('This is my first post')
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
-describe "UsersControllers", type: :request do
-    describe "GET #index" do
-      it "returns a successful response" do
-        get '/users'
-        expect(response).to have_http_status(:success)
-      end
-
-      it "renders the index template" do
-        get '/users'
-        expect(response).to render_template(:index)
-      end
-
-      it "includes correct placeholder text in the response body" do
-        get '/users'
-        expect(response.body).to include("Tom")
-      end
+describe 'UsersControllers', type: :request do
+  describe 'GET #index' do
+    it 'returns a successful response' do
+      get '/users'
+      expect(response).to have_http_status(:success)
     end
 
-    describe "GET #show" do
-      let(:user) { User.create(name: "Ruby Guy") }
-    
-      it "returns a successful response" do
-        get "/users/#{user.id}"
-        expect(response).to have_http_status(:success)
-      end
-
-      it "renders the show template" do
-        get "/users/#{user.id}"
-        expect(response).to render_template(:show)
-      end
-
-      it "includes correct placeholder text in the response body" do
-        get "/users/#{user.id}"
-        expect(response.body).to include('Ruby Guy')
-      end
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template(:index)
     end
+
+    it 'includes correct placeholder text in the response body' do
+      get '/users'
+      expect(response.body).to include('Tom')
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { User.create(name: 'Ruby Guy') }
+
+    it 'returns a successful response' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get "/users/#{user.id}"
+      expect(response.body).to include('Ruby Guy')
+    end
+  end
 end
