f09769ae6e116d85a2021789c9901f660fd82f3c
kifle23 <kiflekifle23@gmail.com>
2023-08-22 12:11:02 +0300

    Spec for PostsController#index->includes placeholder

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,5 +10,10 @@ describe PostsController, type: :request do
         get '/users/1/posts' 
         expect(response).to render_template(:index)
       end
+
+      it 'includes correct placeholder text in the response body' do
+        get '/users/1/posts' 
+        expect(response.body).to include("Tom")
+      end
     end
 end
