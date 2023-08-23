0137fa79c4200ec42427a5f9f5e43133630099b2
kifle23 <kiflekifle23@gmail.com>
2023-08-23 22:13:05 +0300

    Update route for likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create] do
       resources :comments, only: [:create]
+      resources :likes, only: [:create]
     end
   end
   root 'users#index'
