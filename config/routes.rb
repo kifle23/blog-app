56768d2976848b6b5227d0b9257e00641e1537e2
kifle23 <kiflekifle23@gmail.com>
2023-08-23 13:21:11 +0300

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create]
   end
   root 'users#index'
 end
