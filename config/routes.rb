1fcc52450f90a91703a28ebe74afe97ba4befbd1
kifle23 <kiflekifle23@gmail.com>
2023-08-23 19:46:32 +0300

    Add route to comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :create]
+    resources :posts, only: [:index, :show, :create] do
+      resources :comments, only: [:create]
+    end
   end
   root 'users#index'
 end
