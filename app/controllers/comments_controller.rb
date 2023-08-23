cb9ac1962d23e0a8b1d1d82145319046e3f0a5a8
kifle23 <kiflekifle23@gmail.com>
2023-08-23 21:22:37 +0300

    Update Comment to create on behalf of the current_user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,18 @@
 class CommentsController < ApplicationController
     def create
-      post = Post.find(params[:id])
+      post = Post.find(params[:post_id])
+      p post
       comment = current_user.comments.new(comments_params)
       comment.post = post
       if comment.save
         flash[:success] = 'Successfully commented on this post'
-        redirect_to user_post_url
+        redirect_to user_post_url(id: params[:post_id])
       else
         flash.now[:error] = 'Error: Post could not be created'
-        render user_post_path
+        render user_post_path(id: current_user.id)
       end
     end
-  
+
     private
   
     def comments_params
