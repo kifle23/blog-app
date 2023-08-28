ddc56657153c7c7f2068114afc2899b3692d808e
kifle23 <kiflekifle23@gmail.com>
2023-08-28 17:22:32 +0300

    Update development conf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,8 +6,8 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  # config.cache_classes = false
-  config.reload_classes_only_on_change = false
+  config.cache_classes = false
+  # config.reload_classes_only_on_change = false
 
   # Do not eager load code on boot.
   config.eager_load = false
