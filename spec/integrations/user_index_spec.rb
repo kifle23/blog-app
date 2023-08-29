53ab7de9208cdcd5b3e102091404157c8408fa99
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-08-29 12:05:25 +0330

    Fix all the linter errors

--- spec/integrations/user_index_spec.rb
+++ spec/integrations/user_index_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe 'User Index Page Features', type: :feature, js: true do
   before :each do
     @user1 = User.create(
-        name: 'Ruby Guy',
-        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
-        bio: 'I am a user1',
-        posts_count: 5
-      )
+      name: 'Ruby Guy',
+      photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
+      bio: 'I am a user1',
+      posts_count: 5
+    )
   end
 
   it 'can see the username of all other users' do
--- spec/integrations/user_show_spec.rb
+++ spec/integrations/user_show_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'User Show Page Features', type: :feature, js: true do
   before :each do
     @user1 = User.create(
-        name: 'Ruby Guy',
-        photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
-        bio: 'I am a user1',
-        posts_count: 5
-      )
+      name: 'Ruby Guy',
+      photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
+      bio: 'I am a user1',
+      posts_count: 5
+    )
 
-    
+
     Post.create(id: 4, title: 'Post 1', text: 'text 1', likes_counter: 0, comments_counter: 0, author_id: @user1.id)
     Post.create(id: 5, title: 'Post 2', text: 'text 2', likes_counter: 0, comments_counter: 0, author_id: @user1.id)
     Post.create(id: 6, title: 'Post 3', text: 'text 3', likes_counter: 0, comments_counter: 0, author_id: @user1.id)
