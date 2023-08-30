adf9e2d1d1e422ef8fcd944d439e116f39802e2a
kifle23 <kiflekifle23@gmail.com>
2023-08-30 19:15:22 +0300

    Update users schema to include remember_created_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_160339) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_161346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_160339) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.string "unconfirmed_email"
+    t.datetime "remember_created_at"
   end
 
   add_foreign_key "comments", "posts"
