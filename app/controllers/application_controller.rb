faa8d11f037e521b985296954a86fbbee7e4c63a
kifle23 <kiflekifle23@gmail.com>
2023-08-30 12:48:53 +0300

    Remove current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
-  helper_method :current_user
-
-  def current_user
-    @current_user ||= User.first
-  end
-
+  
   private
 
   def set_current_user
