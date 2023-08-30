947c8ef6c19fe343ed18acd15edba47d23bf7a97
kifle23 <kiflekifle23@gmail.com>
2023-08-30 15:45:04 +0300

    Add devise to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 # app/models/user.rb
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
