9182b96726da85ea706458d8f4e63fdb2842415f
kifle23 <kiflekifle23@gmail.com>
2023-08-30 17:03:09 +0300

    Add validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :password, presence: true
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
