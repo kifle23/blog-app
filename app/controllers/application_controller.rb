978353f7a5859e09258c6783023e3422b1ef5c24
kifle23 <kiflekifle23@gmail.com>
2023-08-30 18:57:03 +0300

    Enable adding new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
   helper_method :current_user
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 
   private
 
