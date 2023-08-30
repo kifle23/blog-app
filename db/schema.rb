5c813e217889cbc3d80dad38d1b8d1f60e1e3e96
kifle23 <kiflekifle23@gmail.com>
2023-08-30 19:06:43 +0300

    Update users schema to include unconfirmed_email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_131941) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_160339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_131941) do
     t.datetime "confirmed_at"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.string "unconfirmed_email"
   end
 
   add_foreign_key "comments", "posts"
