271866f5519816cadde269e8f2dce400cb8b3ea7
kifle23 <kiflekifle23@gmail.com>
2023-08-30 15:39:13 +0300

    Update route to use device users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   get 'likes/create'
+
+  devise_for :users
+
+  devise_scope :user do
+    root 'users#index'
+  end
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :likes, only: [:create]
       resources :comments, only: [:new, :create]
     end
   end
-  root 'users#index'
 end
