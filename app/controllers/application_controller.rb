017ae4cff554eb2c2be97084ddac4a225b2972af
kifle23 <kiflekifle23@gmail.com>
2023-08-31 17:52:56 +0300

    Remove current user helper from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, :update_allowed_parameters, if: :devise_controller?
-  helper_method :current_user
-
+  
   protected
 
   def update_allowed_parameters
