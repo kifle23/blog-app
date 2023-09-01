42d251ebc349558695d2300b2bbd8e4715a3e2e7
kifle23 <kiflekifle23@gmail.com>
2023-09-01 12:07:19 +0300

    Implment action for the API endpoints-comments-get

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+      user = User.find(params[:user_id])
+      post = user.posts.find(params[:post_id])
+      comments = post.comments
+      render json: comments
+    end
 end
