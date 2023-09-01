9821ed0b7c63cb2626bdf9b9a27b3c7212ee66d7
Abdul Ali Noori <ab.noori.1993@gmail.com>
2023-09-01 13:51:16 +0330

    Authenticate users to create comment end points.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class Api::V1::CommentsController < ApplicationController
       include RecordNotFoundHandler
+      before_action :authenticate_user!
+      
       def index
         user = User.find(params[:user_id])
         post = user.posts.find(params[:post_id])
