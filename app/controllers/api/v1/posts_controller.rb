6c8cc559e7fb36d354b835eba6f3368a698346f3
kifle23 <kiflekifle23@gmail.com>
2023-09-01 11:51:44 +0300

    Implment action for the API endpoints-posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::PostsController < ApplicationController
+    def index
+        user = User.find(params[:user_id])
+        posts = user.posts
+        render json: posts
+    end
 end
