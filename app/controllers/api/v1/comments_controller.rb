01f162b477866134c5bd93f91a4fd2d773f8d5f2
kifle23 <kiflekifle23@gmail.com>
2023-09-01 12:31:21 +0300

    Implment action for the API endpoints-comments-create

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,4 +5,22 @@ class Api::V1::CommentsController < ApplicationController
       comments = post.comments
       render json: comments
     end
+
+    def create
+      user = User.find(params[:user_id])
+      post = user.posts.find(params[:post_id])
+      comment = post.comments.build(comment_params)
+      comment.user = user
+      if comment.save
+        render json: comment, status: :created
+      else
+        render json: comment.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+    
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
 end
