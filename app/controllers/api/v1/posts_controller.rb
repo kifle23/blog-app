2b20f783f5e7be068e9f1f2fd02c26a935335c76
kifle23 <kiflekifle23@gmail.com>
2023-09-01 13:02:54 +0300

    Integrate record not found handler to post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,12 @@
-class Api::V1::PostsController < ApplicationController
-    def index
-        user = User.find(params[:user_id])
-        posts = user.posts
-        render json: posts
+module Api
+    module V1
+        class Api::V1::PostsController < ApplicationController
+            include RecordNotFoundHandler
+            def index
+                user = User.find(params[:user_id])
+                posts = user.posts
+                render json: posts
+            end
+        end
     end
 end
